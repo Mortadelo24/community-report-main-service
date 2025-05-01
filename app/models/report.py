c307a4ef9dec11bef01ddd7b180fc1e35071b713
Mortadelo24 <josedanielmirandaperez410@gmail.com>
2025-04-30 19:49:39 -0600

    add tiem to reports

--- app/models/report.py
+++ app/models/report.py
@@ -2,19 +2,21 @@ from sqlmodel import SQLModel, Relationship, Field
 from .user import User
 from .community import Community
 from uuid import UUID, uuid4
-
-
+from datetime import datetime
 class ReportBase(SQLModel):
     complaint: str = Field(min_length=8)
+
 
 class ReportCreate(ReportBase):
     community_id: UUID
 
 class ReportPublic(ReportBase):
     id: UUID
+    created_at: datetime
 
 class Report(ReportBase, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
+    created_at: datetime | None = Field(default_factory=datetime.now)
     user: User = Relationship(back_populates="reports")
     community: Community = Relationship(back_populates="reports")
     user_id: UUID = Field(foreign_key="user.id")
